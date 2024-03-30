84443d4727c79e1998cda53f286b68418a227485
SergGrishkov <serg.grishkov@gmail.com>
2024-03-30 13:00:30 +0200

    Add API contacts and App component upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,18 @@ import "../../../node_modules/modern-normalize/modern-normalize.css";
 import { ContactForm } from "../ContactForm/ContactForm";
 import { ContactList } from "../ContactList/ContactList";
 import { SearchBox } from "../SearchBox/SearchBox";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getContacts } from "../../redux/contactsApi";
 import css from "./App.module.css";
 
 export const App = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1 className={css.title}>Phonebook</h1>
